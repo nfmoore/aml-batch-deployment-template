b2ad41f882723e221774092a16fdd93b3ac0cb97
Nicholas Moore <nfmoore@outlook.com>
2020-07-11 17:25:01 +1000

    update tests

--- tests/score_test.py
+++ tests/score_test.py
@@ -2,7 +2,7 @@ from unittest.mock import MagicMock, patch
 
 import pandas as pd
 from tests.fixtures import data
-from tests.mocks import MockModel, MockRunContext
+from tests.mocks import MockModel
 
 from src.score import process_data, run
 
